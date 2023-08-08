235a60fee38bbf91a4efdedddb6d47a5dc69d57e
joshuadavis1990 <joshuadavis1990@me.com>
2023-08-08 13:43:52 +1000

    Added .env

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 async function dbClose() {
   await mongoose.connection.close()
   console.log('Database disconnected')
 }
 
-mongoose.connect('mongodb+srv://joshuadavis1990:7Czg2HSq0IDldvJU@cluster0.djjulcw.mongodb.net/journal?retryWrites=true&w=majority')
+mongoose.connect(process.env.ATLAS_DB_URL)
   .then(m => console.log(m.connection.readyState == 1 ? 'Mongoose connected!' : 'Mongoose failed to connect.'))
   .catch(err => console.log(err))
 
